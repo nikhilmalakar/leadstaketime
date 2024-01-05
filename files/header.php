3d29e61c18b06adace950a53b663430d4514211d
nikhilmalakar <nikhilmalakarofficial@gmail.com>
2024-01-05 17:09:45 +0530

    Fix render bug - Pricing page

--- files/header.php
+++ files/header.php
@@ -20,12 +20,14 @@ if (us_level || (!us_level && in_array($pg, ['survey', 'login-google', 'login-tw
 			height: auto;
 			border-radius: 100%;
 		}
-		@media (min-width: 900px){
-			.dropdown-menu.show{
+
+		@media (min-width: 900px) {
+			.dropdown-menu.show {
 				left: -75px;
 			}
 		}
-		.dropdown-menu.show{
+
+		.dropdown-menu.show {
 			box-shadow: 0 0 9px 3px #d7d7d7;
 		}
 	</style>
@@ -33,7 +35,7 @@ if (us_level || (!us_level && in_array($pg, ['survey', 'login-google', 'login-tw
 
 	<nav class="navbar navbar-expand-lg navbar-light bg-white m-0" style="">
 		<a href="#" class="navbar-brand">
-			<img src="<?= path ?>/assets/img/leadstaketime_logo.png"  height="56" alt="Leads Take Time">
+			<img src="<?= path ?>/assets/img/leadstaketime_logo.png" height="56" alt="Leads Take Time">
 		</a>
 		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02"
 			aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
@@ -57,102 +59,97 @@ if (us_level || (!us_level && in_array($pg, ['survey', 'login-google', 'login-tw
 				</li>
 			</ul>
 
-			<div class="navbar-nav ms-auto">
-				<?php if (us_level != 6): ?>
-					<div class="nav-action-button">
-						<a href="./plans" class="nav-item" style="color:white !important;"><i class="far fa-gem"
-								style="padding-right:10px !important;"></i>Pricing</a>
-					</div>
-				<?php endif; ?>
+				<div class="navbar-nav ms-auto">
+					<?php if (us_level != 6): ?>
+						<div class="nav-action-button">
+							<a href="./plans" class="nav-item" style="color:white !important;"><i class="far fa-gem"
+									style="padding-right:10px !important;"></i>Pricing</a>
+						</div>
+					<?php endif; ?>
 
-				<?php if (!us_level): ?>
-					<div class="nav-action-button">
-						<a href="./mysurveys" style="color:white !important;" class="nav-item">Get Started</a>
-					</div>
-				<?php elseif (us_level == 6): ?>
-					<div class="nav-action-button">
-						<a href="./dashboard.php" style="color:white !important;" class="nav-item">Admin Panel</a>
-					</div>
-				<?php elseif (us_level): ?>
-					<div class="nav-action-button">
-						<a href="./mysurveys" style="color:white !important;" class="nav-item">Dashboard</a>
-					</div>
-				<?php endif; ?>
+					<?php if (!us_level): ?>
+						<div class="nav-action-button">
+							<a href="./mysurveys" style="color:white !important;" class="nav-item">Get Started</a>
+						</div>
+					<?php elseif (us_level == 6): ?>
+						<div class="nav-action-button">
+							<a href="./dashboard.php" style="color:white !important;" class="nav-item">Admin Panel</a>
+						</div>
+					<?php elseif (us_level): ?>
+						<div class="nav-action-button">
+							<a href="./mysurveys" style="color:white !important;" class="nav-item">Dashboard</a>
+						</div>
+					<?php endif; ?>
 
-			</div>
-			<div>
-				<?php if (us_level): ?>
-					<?php if (in_array($pg, ['survey']) && $request == 'su'): ?>
-					<?php else: ?>
-						<li class="nav-item dropdown" style="list-style-type: none;">
+				</div>
+				
+				<div>
+					<?php if (us_level): ?>
+						<?php if (in_array($pg, ['survey']) && $request == 'su'): ?>
+						<?php else: ?>
+							<li class="nav-item dropdown" style="list-style-type: none;">
 
-							<ul style="list-style-type: none; padding:0; margin:0;">
-								<li>
-									<?php if (us_level): ?>
+								<ul style="list-style-type: none; padding:0; margin:0;">
+									<li>
+										<?php if (us_level): ?>
 
-										<div class=" pt-profile-icon nav-link dropdown-toggle m-0" href="#" id="navbarDropdown" role="button"
-												data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-											<div class="pt-thumb m-2"><img src="<?= (us_photo ? path . '/' . us_photo : nophoto) ?>"
-													onerror="this.src='<?= nophoto ?>'" /></div>
-											<div>
-												<?= $lang['menu']['welcome'] ?>
-												<?php if (us_level): ?>,
-													<?= us_username ?> <i class="fas fa-angle-down"></i>
-												<?php endif; ?>
+											<div class=" pt-profile-icon nav-link dropdown-toggle m-0" href="#" id="navbarDropdown"
+												role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+												<div class="pt-thumb m-2"><img src="<?= (us_photo ? path . '/' . us_photo : nophoto) ?>"
+														onerror="this.src='<?= nophoto ?>'" />
+												</div>
+												<div>
+													<?= $lang['menu']['welcome'] ?>
+													<?php if (us_level): ?>,
+														<?= us_username ?> <i class="fas fa-angle-down"></i>
+													<?php endif; ?>
+												</div>
 											</div>
-										</div>
 
-										<div class="dropdown-menu" aria-labelledby="navbarDropdown">
-											<?php if (us_level != 6): ?>
-												<a class="dropdown-item" href="#newSurveyModal" data-toggle="modal"><i
-														class="fas fa-plus"></i>
-													<?= $lang['menu']['new'] ?>
+											<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+												<?php if (us_level != 6): ?>
+													<a class="dropdown-item" href="#newSurveyModal" data-toggle="modal"><i
+															class="fas fa-plus"></i>
+														<?= $lang['menu']['new'] ?>
+													</a>
+												<?php endif; ?>
+												<?php if (us_level == 6): ?>
+													<a class="dropdown-item" href="<?= path ?>/dashboard.php"><i class="fas fa-cogs"></i>
+														<?= $lang['menu']['admin'] ?></a>
+												<?php endif; ?>
+												<div class="dropdown-divider"></div>
+												<a class="dropdown-item" href="<?= path ?>/index.php?pg=userdetails"><i
+														class="fas fa-user-cog"></i>
+													<?= $lang['menu']['info'] ?>
+													<span
+														class="badge <?= (us_plan == '1' ? 'bg-gy' : (us_plan == '2' ? 'bg-gr' : (us_plan == '3' ? 'bg-v' : 'bg-o'))) ?>">
+														<?= (us_plan ? db_get("plans", "plan", us_plan) : $lang['details']['freeplan']) ?>
+													</span>
 												</a>
-											<?php endif; ?>
-											<?php if (us_level == 6): ?>
-												<a class="dropdown-item" href="<?= path ?>/dashboard.php"><i class="fas fa-cogs"></i>
-													<?= $lang['menu']['admin'] ?>
-												</a>
-											<?php endif; ?>
-											<div class="dropdown-divider"></div>
-											<a class="dropdown-item" href="<?= path ?>/index.php?pg=userdetails"><i
-													class="fas fa-user-cog"></i>
-												<?= $lang['menu']['info'] ?>
-												<span
-													class="badge <?= (us_plan == '1' ? 'bg-gy' : (us_plan == '2' ? 'bg-gr' : (us_plan == '3' ? 'bg-v' : 'bg-o'))) ?>">
-													<?= (us_plan ? db_get("plans", "plan", us_plan) : $lang['details']['freeplan']) ?>
-												</span>
-											</a>
-											<div class="dropdown-divider"></div>
-											<!-- <li><a href="#" class="pt-logout"><i class="fas fa-power-off"></i> <?=$lang['menu']['logout']?></a></li> -->
-						
-											<a class="dropdown-item pt-logout" href="#" class=""><i class="fas fa-power-off"></i>
-												<?= $lang['menu']['logout'] ?>
-											</a>
-										</div>
+												<div class="dropdown-divider"></div>
+												<!-- <li><a href="#" class="pt-logout"><i class="fas fa-power-off"></i> <?= $lang['menu']['logout'] ?></a></li> -->
 
-									<?php endif; ?>
-								</li>
-								<?php if (!us_level): ?>
-									<li><a href="#loginModal" data-toggle="modal" class="pt-btn"><i class="far fa-user"></i>
-											<?= $lang['menu']['signin'] ?>
-										</a>
+												<a class="dropdown-item pt-logout" href="#" class=""><i class="fas fa-power-off"></i>
+													<?= $lang['menu']['logout'] ?></a>
+											</div>
+
+										<?php endif; ?>
 									</li>
-								<?php endif; ?>
-							</ul>
-
-
-						</li>
-
-
+									<?php if (!us_level): ?>
+										<li><a href="#loginModal" data-toggle="modal" class="pt-btn"><i class="far fa-user"></i>
+												<?= $lang['menu']['signin'] ?></a>
+										</li>
+									<?php endif; ?>
+								</ul>
+							</li>
+						<?php endif; ?>
 					<?php endif; ?>
 				</div>
-			</div>
-		</nav>
+		</div>
+	</nav>
 
 
 
-	<?php endif; ?>
 
 
 	<!-- The Modal -->
